2162c8d81e005b037775a609d7cfbd9a95528ff4
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2024-01-14 19:06:49 +0100

    ci: set build folder back to _site

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/admin/config.yml");
   eleventyConfig.addPassthroughCopy("src/static");
 
-  // Input Directory : src Output directory: docs
+  // Input Directory : src Output directory: _site
   return {
-    dir: { input: "src", output: "docs" },
+    dir: { input: "src", output: "_site" },
   };
 };
